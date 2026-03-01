2a47f665bcedcdcba419f6d14cd039ffaa24ac69
al-nf <alankjfung@gmail.com>
2026-03-01 03:54:12 -0800

    OOPS

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,20 +1,20 @@
 <script lang="js">
-    import 'ol/ol.css';
+    import "ol/ol.css";
 
-    import Map from 'ol/Map.js';
-    import View from 'ol/View.js';
-    import KML from 'ol/format/KML.js';
-    import HeatmapLayer from 'ol/layer/Heatmap.js';
-    import TileLayer from 'ol/layer/Tile.js';
-    import StadiaMaps from 'ol/source/StadiaMaps.js';
-    import VectorSource from 'ol/source/Vector.js';
-    import {fromLonLat} from 'ol/proj.js';
-    import {onMount} from 'svelte';
-    import { SimpleGeometry } from 'ol/geom';
+    import Map from "ol/Map.js";
+    import View from "ol/View.js";
+    import KML from "ol/format/KML.js";
+    import HeatmapLayer from "ol/layer/Heatmap.js";
+    import TileLayer from "ol/layer/Tile.js";
+    import StadiaMaps from "ol/source/StadiaMaps.js";
+    import VectorSource from "ol/source/Vector.js";
+    import { fromLonLat } from "ol/proj.js";
+    import { onMount } from "svelte";
+    import { SimpleGeometry } from "ol/geom";
 
     //debug
-    import VectorLayer from 'ol/layer/Vector.js';
-import {Style, Circle, Fill} from 'ol/style.js';
+    import VectorLayer from "ol/layer/Vector.js";
+    import { Style, Circle, Fill } from "ol/style.js";
 
     // if there's a place u gotta go, im the one you needta know, im the map, im the map im the map
     let mapId = 20;
@@ -28,7 +28,7 @@ import {Style, Circle, Fill} from 'ol/style.js';
     let coordLat = 0.0;
     let coordLon = 0.0;
     // let backend = http://129.212.186.70
-    let backend = "https://team3-2.solarflare-godzilla.ts.net"
+    let backend = "https://team3-2.solarflare-godzilla.ts.net";
 
     let source = new VectorSource();
 
@@ -36,12 +36,11 @@ import {Style, Circle, Fill} from 'ol/style.js';
         source: source,
         blur: 60,
         radius: 50,
-        weight: f => {
-            const val = parseFloat(f.get('description')) || 0;
-            return val * 5;  // amplify
-        }
+        weight: (f) => {
+            const val = parseFloat(f.get("description")) || 0;
+            return val * 5; // amplify
+        },
     });
-
 
     const setupMap = (node) => {
         console.log("hey vro");
@@ -49,22 +48,18 @@ import {Style, Circle, Fill} from 'ol/style.js';
 
         const raster = new TileLayer({
             source: new StadiaMaps({
-              layer: 'osm_bright',
-            })
-        })
-
+                layer: "osm_bright",
+            }),
+        });
 
         map = new Map({
             target: node,
-            layers: [
-                raster,
-                vector
-            ],
+            layers: [raster, vector],
             view: new View({
-                projection: 'EPSG:3857',
+                projection: "EPSG:3857",
                 center: fromLonLat([0, 0]),
                 zoom: 2,
-            })
+            }),
         });
 
         return {
@@ -73,64 +68,58 @@ import {Style, Circle, Fill} from 'ol/style.js';
                     map.setTarget(null);
                     map = null;
                 }
-            }
-        }
-    }
+            },
+        };
+    };
 
     const updateMap = (node) => {
         console.log("yesss vro");
         console.log(lat);
         console.log(lon);
 
-        if(lat != null) coordLat = lat;
-        if(lon != null) coordLon = lon;
+        if (lat != null) coordLat = lat;
+        if (lon != null) coordLon = lon;
 
-        map.getView().animate(
-            {
-                zoom: 9.5,
-                center: fromLonLat([coordLon, coordLat]),
-                duration: 750,
-            }
-        );
+        map.getView().animate({
+            zoom: 9.5,
+            center: fromLonLat([coordLon, coordLat]),
+            duration: 750,
+        });
 
-
-
-        fetch(`${}/get-map?latitude=${coordLat}&longitude=${coordLon}`)
-            .then(r => r.text())
-            .then(kmlData => {
-
+        fetch(`${backend}/get-map?latitude=${coordLat}&longitude=${coordLon}`)
+            .then((r) => r.text())
+            .then((kmlData) => {
                 const format = new KML();
                 const features = format.readFeatures(kmlData, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:4326' // keep in lon/lat for flipping later
+                    dataProjection: "EPSG:4326",
+                    featureProjection: "EPSG:4326", // keep in lon/lat for flipping later
                 });
 
-                    console.log("features:", features.length);
-                    console.log(source);
-                    console.log(map.getLayers().getLength());
+                console.log("features:", features.length);
+                console.log(source);
+                console.log(map.getLayers().getLength());
 
                 // KML vs OpenLayers: lon/lat order convention
-                features.forEach(feature => {
+                features.forEach((feature) => {
                     const geom = feature.getGeometry();
-                    if(geom instanceof SimpleGeometry) {
-                        if(geom.getType() == 'Point') {
+                    if (geom instanceof SimpleGeometry) {
+                        if (geom.getType() == "Point") {
                             const coords = geom.getCoordinates();
                             geom.setCoordinates([coords[1], coords[0]]);
                         }
                     }
-                })
-                features.forEach(f => {
-                    f.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-                })
+                });
+                features.forEach((f) => {
+                    f.getGeometry().transform("EPSG:4326", "EPSG:3857");
+                });
 
                 source.clear();
                 source.addFeatures(features);
 
-                    // map.getView().fit(source.getExtent(), {
-                    //    padding: [50,50,50,50]
-                    //});
-            })
-
+                // map.getView().fit(source.getExtent(), {
+                //    padding: [50,50,50,50]
+                //});
+            });
 
         return {
             destroy() {
@@ -138,17 +127,12 @@ import {Style, Circle, Fill} from 'ol/style.js';
                     map.setTarget(null);
                     map = null;
                 }
-            }
-        }
-    }
+            },
+        };
+    };
 
-    const checkLat = () => {
-
-    }
-    const checkLon = () => {
-
-    }
-
+    const checkLat = () => {};
+    const checkLon = () => {};
 </script>
 
 <link rel="stylesheet" href="node_modules/ol/ol.css" />

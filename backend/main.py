fb772754058f5cb9597de2930774e460719a3d39
al-nf <alankjfung@gmail.com>
2026-02-28 18:14:45 -0800

    fixed mrgconflict

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
+import simplekml
 from flask import Flask, make_response
 from flask_cors import CORS
-import simplekml
+
 # import firebase_admin
 # from firebase_admin import credentials, firestore
 
@@ -16,14 +17,9 @@ def hello_world():
 @app.route("/get-map", methods=["GET"])
 def get_map():
     kml = simplekml.Kml()
-<<<<<<< HEAD
-    pnt = kml.newpoint(
-        name="dummy", coords=[(37.3483333333, -121.9353888889)], description="10"
+
+    kml.newpoint(
+        name="dummy", coords=[(37.3483333333, -121.9353888889)], description="0.5"
     )
+
     return make_response(kml.kml(), 200, {"Content-Type": "application/xml"})
-=======
-
-    kml.newpoint(name="dummy", coords=[(37.3483333333, -121.9353888889)], description="0.5")
-
-    return make_response(kml.kml(), 200, {'Content-Type': 'application/xml'})
->>>>>>> 457d152472b9b3170f83434c4a0715bfd7d71205

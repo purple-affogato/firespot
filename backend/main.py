472a01c7e4470756a9e40929c3131762fc10504f
purple-affogato <ivy.yiting.zhuang@gmail.com>
2026-02-28 16:26:04 -0800

    backend: Added CORS
    This way the frontend can request data from the backend.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, make_response
+from flask_cors import CORS
 import simplekml
-import firebase_admin
-from firebase_admin import credentials, firestore
+# import firebase_admin
+# from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def hello_world():
@@ -13,13 +15,6 @@ def hello_world():
 def get_map():
     kml = simplekml.Kml()
     
-    cred = credentials.Certificate("firespot-331a1-firebase-adminsdk-fbsvc-43fc2da019.json")
-    firebase_admin.initialize_app(cred)
-    
-    db = firestore.client()
-    docs = db.collection("firepoints").stream()
-    
-    for d in docs:
-        kml.newpoint(name=d.get('name'), coords=[(d.get('latitude'), d.get('longitude'))], description=str(d.get('intensity')))
+    kml.newpoint(name="dummy", coords=[(37.3483333333, -121.9353888889)], description="0.5")
 
     return make_response(kml.kml(), 200, {'Content-Type': 'application/xml'})
